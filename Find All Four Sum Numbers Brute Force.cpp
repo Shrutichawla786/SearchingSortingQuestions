ab6b22824f5f216589b43a9c53e37cec36c63fa5
shruti chawla <114687114+Shrutichawla786@users.noreply.github.com>
2024-02-15 13:04:54 +0530

    Find All Four Sum Numbers Brute Force

---
+++ Find All Four Sum Numbers Brute Force.cpp
@@ -0,0 +1,34 @@
+Q.Find All Four Sum Numbers
+Link-->https://www.geeksforgeeks.org/problems/find-all-four-sum-numbers1732/1
+code--> vector<vector<int> > fourSum(vector<int> &nums, int target) {
+        // Your code goes here<
+        int n = nums.size(); //size of the array
+    set<vector<int>> st;
+
+    //checking all possible quadruplets:
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                for (int l = k + 1; l < n; l++) {
+                    // taking bigger data type
+                    // to avoid integer overflow:
+                    long long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+
+                    if (sum == target) {
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
+                    }
+                }
+            }
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+    
+    }
+T.C-->O(N^4)
+  S.C-->O(quart)*2
+  
