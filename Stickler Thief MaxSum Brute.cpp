7e5d00c4967c6d1db3baaa0f4f0ed33106022adf
shruti chawla <114687114+Shrutichawla786@users.noreply.github.com>
2024-02-16 12:06:08 +0530

    Stickler Thief MaxSum Brute

--- Stickler Thief MaxSum Brute.cpp
+++ Stickler Thief MaxSum Brute.cpp
@@ -1,27 +1,21 @@
 Q.Stickler Thief
 link--->https://www.geeksforgeeks.org/problems/stickler-theif-1587115621/1
-code-->int solve(int arr[] , int n , vector<int>&dp){
+code-->int solve(int arr[] , int n){
         if(n<0){
             return 0;
         }
         if(n==0){
             return arr[0];
         }
-        if(dp[n] !=-1){
-            return dp[n];
-        }
-        int inc= solve(arr , n-2 , dp)+arr[n];
-        int excl= solve(arr , n-1 , dp)+0;
-        
-        dp[n]=max(inc , excl);
-        return dp[n];
+        int inc= solve(arr , n-2)+arr[n];
+        int excl= solve(arr , n-1)+0;
+        return max(inc , excl);
         
     }
     int FindMaxSum(int arr[], int n)
     {
         // Your code here
-        vector<int>dp(n , -1);
-        return solve(arr , n-1 , dp);
+        return solve(arr , n-1);
     }
 Expected Time Complexity:O(exponitial).
 Expected Space Complexity:O(1).
